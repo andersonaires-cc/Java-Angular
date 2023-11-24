d0f1523d5a97f7c03dea2b75e3859b895a1e6d1c
anderson <andersonairesspp@hotmail.com>
2023-11-23 22:37:13 -0300

    Autowired

--- api/src/main/java/br/com/projeto/api/controle/Controle.java
+++ api/src/main/java/br/com/projeto/api/controle/Controle.java
@@ -1,11 +1,17 @@
 package br.com.projeto.api.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.api.repositorio.Repositorio;
 
 @RestController
 public class Controle {
     
+    @Autowired
+    private Repositorio acao;
+
     @GetMapping("/")
     public String teste(){
         return "Hello Wolrd!";
